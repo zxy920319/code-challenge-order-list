3a5eea599e0f587ed61a862c3aa167e5a001299e
Bevin <bevin@provisionvehiclecameras.com>
2020-01-27 22:48:56 +0000

    add language settings

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,25 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-menu bottom left>
+        <template v-slot:activator="{ on }">
+          <v-btn small fab v-on="on">
+            <v-avatar color="secondary" size="36">
+              <span class="white--text">{{ selectedLang }}</span>
+            </v-avatar>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in langs"
+            :key="i"
+            @click="setDefaultLang(item.title)"
+          >
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-content>
@@ -40,6 +51,12 @@ export default {
   },
 
   data: () => ({
+    langs: [
+      { title: "EN", name: "English" },
+      { title: "ZH", name: "中文" },
+      { title: "JA", name: "日本語" }
+    ],
+    selectedLang: "EN"
     //
   }),
 
@@ -47,6 +64,29 @@ export default {
     // eslint-disable-next-line no-console
     console.warn(this);
     GetUserInfo();
+  },
+
+  methods: {
+    setDefaultLang(title) {
+      switch (title) {
+        case "EN":
+          this.$vuetify.lang.current = "en";
+          break;
+
+        case "ZH":
+          this.$vuetify.lang.current = "zhHans";
+          break;
+
+        case "JA":
+          this.$vuetify.lang.current = "ja";
+          break;
+
+        default:
+          this.$vuetify.lang.current = "en";
+          break;
+      }
+      this.selectedLang = title;
+    }
   }
 };
 </script>
