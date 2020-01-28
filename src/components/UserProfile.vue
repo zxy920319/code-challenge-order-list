334ea3653c47222ab590164f326284777a7d5fc0
Bevin <bevin@provisionvehiclecameras.com>
2020-01-28 22:54:03 +0000

    add user profile

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -15,20 +15,45 @@
             height="100vh"
             width="80vw"
           >
-            <div class="picture">
-              <v-btn fab color="primary" elevation="4">
-                <v-avatar size="150">
-                  <img
-                    :src="profile.picture.large"
-                    :alt="`${profile.firstName} ${profile.lastName}`"
-                  />
-                </v-avatar>
-              </v-btn>
-            </div>
-            <v-row justify="center" class="mt-4">
-              <div class="headline accent--text text--darken-4">
-                {{ name }}
-              </div>
+            <v-row justify="center" align="center">
+              <v-col cols="12" md="4">
+                <div class="picture">
+                  <v-btn fab color="primary" elevation="4">
+                    <v-avatar size="150">
+                      <img :src="profile.picture.large" :alt="name" />
+                    </v-avatar>
+                  </v-btn>
+                </div>
+                <v-col
+                  cols="12"
+                  md="12"
+                  class="d-flex justify-center align-center  headline accent--text text--darken-4"
+                >
+                  {{ name }}
+                  <v-icon v-if="profile.gender === 'Female'" color="pink"
+                    >mdi-gender-female</v-icon
+                  >
+                  <v-icon v-else color="blue">mdi-gender-male</v-icon>
+                </v-col>
+                <div
+                  class="d-flex justify-center align-center mt-6 headline accent--text text--darken-4"
+                >
+                  <v-icon small color="primary" class="mr-4">mdi-email</v-icon>
+                  <div class="body-1 mt-1">{{ profile.emailAddress }}</div>
+                </div>
+                <div
+                  class="d-flex justify-center align-center mt-4 headline accent--text text--darken-4"
+                >
+                  <v-icon small color="primary" class="mr-4">mdi-cake</v-icon>
+                  <div class="body-1 mt-1">{{ dob }}</div>
+                </div>
+                <div
+                  class="d-flex justify-center align-center mt-4 headline accent--text text--darken-4"
+                >
+                  <v-icon small color="primary" class="mr-4">mdi-home</v-icon>
+                  <div class="body-1 mt-1">{{ profile.address }}</div>
+                </div>
+              </v-col>
             </v-row>
           </v-sheet>
         </v-col>
@@ -40,7 +65,7 @@
 <script>
 import { GetUserInfo } from "@/services";
 import { mapState, mapMutations } from "vuex";
-
+import * as moment from "moment";
 export default {
   name: "UserProfile",
 
@@ -52,6 +77,9 @@ export default {
     ...mapState(["profile"]),
     name() {
       return `${this.profile.firstName} ${this.profile.lastName}`;
+    },
+    dob() {
+      return moment(this.profile.birthday).format("D MMM YYYY");
     }
   },
 
